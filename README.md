b6babd39f86b78c5ddfc36923e202eca6c4425e9
Daniel <d15_1_89@msn.com>
2018-05-26 01:15:30 +0100

    Created function and imported libs

---
+++ README.md
@@ -0,0 +1,3 @@
+Setup: https://capacitor.ionicframework.com/docs/getting-started/with-ionic/
+
+Demo: https://www.youtube.com/watch?v=nQSpMO6N0tc&feature=youtu.be
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { Plugins, CameraResultType, CameraSource } from "@capacitor/core";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  image: SafeResourceUrl;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private domSanitizer: DomSanitizer) {}
 
+  async takePhoto() {
+    const { Camera } = Plugins;
+    const result = await Camera.getPhoto({
+      quality: 75,
+      allowEditing: true,
+      source: CameraSource.Camera,
+      resultType: CameraResultType.Base64 // change for prod
+    });
+
+    this.image = this.domSanitizer.bypassSecurityTrustResourceUrl(
+      result && result.base64Data
+    );
   }
-
 }
